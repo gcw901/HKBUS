0fc7f9c367c867205001e7fb177cb5bf1c81f919
gcw901 <goldcity.w@gmail.com>
2026-07-10 19:38:21 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hk-bus-eta-current-op-label-v1';
+const CACHE='hk-bus-eta-ctb118-merge-fix-v1';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
